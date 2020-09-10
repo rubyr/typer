507bcc53cba62d1d8cee9315c68cc2b813576532
rubyr <rubyrinken@gmail.com>
2020-09-10 11:31:27 -0600

    Improve timer visuals

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import css from "./Clock.module.css";
 
-const Clock = ({ endTime }) => {
-  const [width, setWidth] = useState();
+const Clock = ({ endTime, score }) => {
+  const [height, setHeight] = useState();
   useEffect(() => {
+    if (endTime === undefined) return;
     const timeInterval = setInterval(() => {
-      setWidth(endTime - Date.now());
+      setHeight(endTime - Date.now());
     }, 1000 / 60);
     return () => {
       clearInterval(timeInterval);
     };
   }, [endTime]);
-  return <div className={css.Clock} style={{ width: width }}></div>;
+  return (
+    <div
+      className={css.Clock}
+      style={{ height: height / (50 / (score / 10 + 1)) + "%" }}
+    >
+      {" "}
+    </div>
+  );
 };
 
 export default Clock;
