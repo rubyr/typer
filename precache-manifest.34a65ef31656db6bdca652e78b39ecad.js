1b0589c979ebc59d6bea1f816adc444798300cb2
rubyr <rubyrinken@gmail.com>
2020-09-10 12:52:09 -0600

    Updates

---
+++ precache-manifest.34a65ef31656db6bdca652e78b39ecad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e86e23671dbe67e218b02f35a54307d",
+    "url": "/typer/index.html"
+  },
+  {
+    "revision": "d6a3ea092aeabb50547d",
+    "url": "/typer/static/css/main.2710135c.chunk.css"
+  },
+  {
+    "revision": "efa74f44e577ab96bacc",
+    "url": "/typer/static/js/2.9427890c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typer/static/js/2.9427890c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a3ea092aeabb50547d",
+    "url": "/typer/static/js/main.25846329.chunk.js"
+  },
+  {
+    "revision": "20baf098e304318156a5",
+    "url": "/typer/static/js/runtime-main.371fae97.js"
+  }
+]);
